2939aeb24ebe319e60c845448d461cdbccdc52b0
junxxx <hprjunxxx@gmail.com>
2024-04-03 22:01:34 +0800

    class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Person
+{
+private:
+    int age;
+
+public:
+    string name;
+
+    Person(string name)
+    {
+        this->name = name;
+    }
+
+    string getName()
+    {
+        return this->name;
+    }
+
+    ~Person()
+    {
+        cout << "Destructor";
+    }
+};
 
 int main(int argc, char const *argv[])
 {
-    cout << "Hello, world!";
+    string name = "Panduola";
+    Person user(name);
+    cout << "Hello, world!" << endl;
+    cout << user.getName() << endl;
     return 0;
 }
